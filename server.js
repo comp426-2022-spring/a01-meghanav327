cb87c6a8f99cd1174bd28d9f73f50efdc9d5ca5c
meghanav327 <meghanav@live.unc.edu>
2022-04-27 13:49:38 -0400

    server

---
+++ server.js
@@ -0,0 +1,24 @@
+const https = require('https')
+const fs = require('fs')
+const args = require('minimist')(process.argv.slice())
+args['port']
+const port = args.port || process.env.port || 3000
+
+
+fs.readFile('./www/index.html', 'utf8', (err, data) => {
+    if (err) {
+        console.error(err)
+        return
+        process.exit(1)
+    }
+
+    const server = http.createServer((req, res) => {
+        res.statusCode = 200
+        res.setHeader('Content-Type', 'text/html')
+        res.end(data)
+    })
+
+    server.listen(port, () => {
+        console.log('Server running at port ${port}')
+    })
+})
